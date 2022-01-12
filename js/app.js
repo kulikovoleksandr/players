136f4b19b11ac8fba13cd4f723135d8ef2404acf
Alex <alexandr.webwork@gmail.com>
2022-01-12 21:12:07 +0200

    1.0.1

---
+++ js/app.js
@@ -0,0 +1,5 @@
+import State from './state.js';
+
+const state = State;
+
+console.log(state);
---
+++ js/state.js
@@ -0,0 +1,34 @@
+function getRandomString(length) {
+  let randomChars = 'abcdefghijklmnopqrstuvwxyz';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += randomChars.charAt(
+      Math.floor(Math.random() * randomChars.length)
+    );
+  }
+  return result.charAt(0).toUpperCase() + result.slice(1);
+}
+
+function getRandomNumber(min, max) {
+    let result;
+    result = Math.random() * (max - min) + min;
+    return Math.floor(result);
+}
+
+class Player {
+  constructor() {
+    this.name = getRandomString(5);
+    this.params = {
+      height: getRandomNumber(160, 210),
+      weight: getRandomNumber(70, 150),
+    };
+  }
+}
+
+let state = [];
+
+for(let i = 0; i < 50; i++){
+    state.push(new Player())
+}
+
+export default state;
