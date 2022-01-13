7a6daf9b861aa5e3eea3e0aec16f06970753cddc
Roman <romanchebotarenko@gmail.com>
2022-01-13 17:48:24 +0200

    1.1.3
    Small refactoring

--- js/app.js
+++ js/app.js
@@ -11,70 +11,49 @@ const state = State;
 const sortedByHeight = _.sortBy(state, ["params.height"]); // мелкий > высокий
 const sortedByHeightReversed = _.reverse(_.sortBy(state, ["params.height"])); // высокий > мелкий
 
-//показать мелких
-btnSmall.onclick = () => {
-  resultList.textContent = "";
-  const resultHeader = document.createElement("h6");
-  resultList.append(resultHeader);
-  resultHeader.textContent = "Cамый мелкий:";
-  for (let i = 0; i < sortedByHeight.length; i++) {
-    if (sortedByHeight[0].params.height === sortedByHeight[i].params.height) {
-      const newResultP = document.createElement("p");
-      newResultP.textContent = `${sortedByHeight[i].name}. Рост: ${sortedByHeight[i].params.height} см`;
-      resultList.append(newResultP);
-    }
-  }
-};
-
-//показать высоких
-btnTall.onclick = () => {
-  resultList.textContent = "";
-  const resultHeader = document.createElement("h6");
-  resultList.append(resultHeader);
-  resultHeader.textContent = "Cамый высокий:";
-  for (let i = 0; i < sortedByHeight.length; i++) {
-    if (
-      sortedByHeightReversed[0].params.height ===
-      sortedByHeightReversed[i].params.height
-    ) {
-      const newResultP = document.createElement("p");
-      newResultP.textContent = `${sortedByHeightReversed[i].name}. Рост: ${sortedByHeightReversed[i].params.height} см`;
-      resultList.append(newResultP);
-    }
-  }
-};
-
 //сортировка по весу
 const sortedByWeight = _.sortBy(state, ["params.weight"]); // худой > толстый
 const sortedByWeightReversed = _.reverse(_.sortBy(state, ["params.weight"])); // толстый > худой
 
-//показать тощих
-btnThin.onclick = () => {
+const showBtnClickContent = (descTxt, sortArray, sortParam, sortParamTxt, measure) => {
   resultList.textContent = "";
   const resultHeader = document.createElement("h6");
   resultList.append(resultHeader);
-  resultHeader.textContent = "Cамый тощий:";
-  for (let i = 0; i < sortedByWeight.length; i++) {
-    if (sortedByWeight[0].params.weight === sortedByWeight[i].params.weight) {
+  resultHeader.textContent = descTxt;
+  
+  for (let i = 0; i < sortArray.length; i++) {
+    if (sortArray[0].params[sortParam] === sortArray[i].params[sortParam]) {
       const newResultP = document.createElement("p");
-      newResultP.textContent = `${sortedByWeight[i].name}. Вес: ${sortedByWeight[i].params.weight} кг`;
+      newResultP.textContent = `${sortArray[i].name}. ${sortParamTxt} ${sortArray[i].params[sortParam]} ${measure}`;
       resultList.append(newResultP);
     }
   }
+}
+
+//показать мелких
+btnSmall.onclick = () => {
+  showBtnClickContent("Cамый мелкий:", sortedByHeight, "height", "Рост:", "см")
+}
+
+//показать высоких
+btnTall.onclick = () => {
+  showBtnClickContent("Cамый высокий:", sortedByHeightReversed, "height", "Рост:", "см")
+};
+
+//показать тощих
+btnThin.onclick = () => {
+  showBtnClickContent("Cамый тощий:", sortedByWeight, "weight", "Вес:", "кг")
 };
 
 
 //показать толстых
 btnFat.onclick = () => {
-  resultList.textContent = "";
-  const resultHeader = document.createElement("h6");
-  resultList.append(resultHeader);
-  resultHeader.textContent = "Cамый толстый:";
-  for (let i = 0; i < sortedByWeightReversed.length; i++) {
-    if (sortedByWeightReversed[0].params.weight === sortedByWeightReversed[i].params.weight) {
-      const newResultP = document.createElement("p");
-      newResultP.textContent = `${sortedByWeightReversed[i].name}. Вес: ${sortedByWeightReversed[i].params.weight} кг`;
-      resultList.append(newResultP);
-    }
-  }
+  showBtnClickContent("Cамый тощий:", sortedByWeightReversed, "weight", "Вес:", "кг")
 };
+
+
+
+
+
+  
+  
