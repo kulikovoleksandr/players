e2460ab914ba0e8cf5a0962c49c808d543a32ff3
Alex <alexandr.webwork@gmail.com>
2022-01-14 10:35:24 +0200

    Update app.js
    vanilla js sort (result in console)

--- js/app.js
+++ js/app.js
@@ -7,16 +7,60 @@ const btnFat = document.getElementById("btn-fat");
 
 const state = State;
 
-console.dir(state);
-
-//get maximum height
-let maxHeight = 0;
-for(let i = 0; i < state.length; i++){
-  if(state[i].params.height > maxHeight){
-    maxHeight = state[i].params.height;
-  } 
+//sort function
+let nestedSort = (prop1, prop2 = null, direction = "asc") => (e1, e2) => {
+  const a = prop2 ? e1[prop1][prop2] : e1[prop1],
+      b = prop2 ? e2[prop1][prop2] : e2[prop1],
+      sortOrder = direction === "asc" ? 1 : -1
+  return (a < b) ? -sortOrder : (a > b) ? sortOrder : 0;
 }
-console.log(maxHeight);
+
+//clone state array
+let heightLowToHigh = [...state];
+let heightHighToLow = [...state];
+
+//sort cloned states
+heightLowToHigh.sort(nestedSort("params", "height", "asc"));
+heightHighToLow.sort(nestedSort("params", "height", "desc"));
+
+//get maximum and minimum height
+const lowest = heightLowToHigh[0].params.height;
+const highest = heightHighToLow[0].params.height;
+
+//get lowest players
+const lowestPlayers = [];
+for(let i = 0; i < heightLowToHigh.length; i++){
+  if(heightLowToHigh[i].params.height === lowest){
+    lowestPlayers.push(heightLowToHigh[i]);
+  } else {
+    break;
+  }
+}
+
+//get highest players
+const highestPlayers = [];
+for(let i = 0; i < heightHighToLow.length; i++){
+  if(heightHighToLow[i].params.height === highest){
+    highestPlayers.push(heightHighToLow[i]);
+  } else {
+    break;
+  }
+}
+
+console.log(`lowest height - ${lowest}`);
+console.log(`highest height - ${highest}`);
+
+console.log('\n"low to high" players:');
+console.dir(heightLowToHigh);
+
+console.log('\n"hight to low" players:');
+console.dir(heightHighToLow);
+
+console.log('\nlowest players:');
+console.dir(lowestPlayers);
+
+console.log('\nhighest players:');
+console.dir(highestPlayers);
 
 
 //сортировка по росту
@@ -27,12 +71,18 @@ const sortedByHeightReversed = _.reverse(_.sortBy(state, ["params.height"])); //
 const sortedByWeight = _.sortBy(state, ["params.weight"]); // худой > толстый
 const sortedByWeightReversed = _.reverse(_.sortBy(state, ["params.weight"])); // толстый > худой
 
-const showBtnClickContent = (descTxt, sortArray, sortParam, sortParamTxt, measure) => {
+const showBtnClickContent = (
+  descTxt,
+  sortArray,
+  sortParam,
+  sortParamTxt,
+  measure
+) => {
   resultList.textContent = "";
   const resultHeader = document.createElement("h6");
   resultList.append(resultHeader);
   resultHeader.textContent = descTxt;
-  
+
   for (let i = 0; i < sortArray.length; i++) {
     if (sortArray[0].params[sortParam] === sortArray[i].params[sortParam]) {
       const newResultP = document.createElement("p");
@@ -40,32 +90,36 @@ const showBtnClickContent = (descTxt, sortArray, sortParam, sortParamTxt, measur
       resultList.append(newResultP);
     }
   }
-}
+};
 
 //показать мелких
 btnSmall.onclick = () => {
-  showBtnClickContent("Cамый мелкий:", sortedByHeight, "height", "Рост:", "см")
-}
+  showBtnClickContent("Cамый мелкий:", sortedByHeight, "height", "Рост:", "см");
+};
 
 //показать высоких
 btnTall.onclick = () => {
-  showBtnClickContent("Cамый высокий:", sortedByHeightReversed, "height", "Рост:", "см")
+  showBtnClickContent(
+    "Cамый высокий:",
+    sortedByHeightReversed,
+    "height",
+    "Рост:",
+    "см"
+  );
 };
 
 //показать тощих
 btnThin.onclick = () => {
-  showBtnClickContent("Cамый тощий:", sortedByWeight, "weight", "Вес:", "кг")
+  showBtnClickContent("Cамый тощий:", sortedByWeight, "weight", "Вес:", "кг");
 };
-
 
 //показать толстых
 btnFat.onclick = () => {
-  showBtnClickContent("Cамый тощий:", sortedByWeightReversed, "weight", "Вес:", "кг")
+  showBtnClickContent(
+    "Cамый тощий:",
+    sortedByWeightReversed,
+    "weight",
+    "Вес:",
+    "кг"
+  );
 };
-
-
-
-
-
-  
-  
