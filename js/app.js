e67e821ad4f4660c515bf4cac85f5b3ab9621046
Roman <romanchebotarenko@gmail.com>
2022-01-15 20:03:40 +0200

    1.0.1
    bug fix

--- js/app.js
+++ js/app.js
@@ -4,6 +4,19 @@ const searchBtn = document.getElementById("search-btn");
 
 const state = State;
 
+ const testArray1 = [
+   { name: "John",
+     property: { lastName: "J", age: 18 }
+   },
+   { name: "Bob",
+     property: { lastName: "B", age: 20 }
+   },
+
+   { name: "Bill",
+     property: { lastName: "L", age: 22 }
+   }
+]; 
+console.log(testArray1)
 
 let nestedSort =
   (parentParamKey, searchParamKey = null, direction = "asc") =>
@@ -39,12 +52,12 @@ const startSort = (
   sortingArray.sort(nestedSort(parentParamKey, searchParamKey, direction));
   for (let i = 0; i < sortingArray.length; i++) {
     if (
-      sortingArray[i].params[searchParamKey] ===
-      sortingArray[0].params[searchParamKey]
+      sortingArray[i][parentParamKey][searchParamKey] ===
+      sortingArray[0][parentParamKey][searchParamKey]
     ) {
       result.push(sortingArray[i]);
       const newResultP = document.createElement("p");
-      newResultP.textContent = `${sortingArray[i].name}. ${sortingArray[i].params[searchParamKey]} ${measure}`;
+      newResultP.textContent = `${sortingArray[i].name}. ${sortingArray[i][parentParamKey][searchParamKey]} ${measure}`;
       resultList.append(newResultP);
     }
   }
