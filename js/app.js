627b302096720ea202e51d2cdd9d73a46ffc9be4
Roman <romanchebotarenko@gmail.com>
2022-01-15 19:56:55 +0200

    1.0 search

--- js/app.js
+++ js/app.js
@@ -1,54 +1,61 @@
 import State from "./state.js";
 const resultList = document.getElementById("result-list");
-const btnShowSmall = document.getElementById("btn-small");
-const btnShowTall = document.getElementById("btn-tall");
-const btnShowThin = document.getElementById("btn-thin");
-const btnShowFat = document.getElementById("btn-fat");
+const searchBtn = document.getElementById("search-btn");
 
 const state = State;
+
 
 let nestedSort =
   (parentParamKey, searchParamKey = null, direction = "asc") =>
   (e1, e2) => {
-    const a = searchParamKey ? e1[parentParamKey][searchParamKey] : e1[parentParamKey],
-      b = searchParamKey ? e2[parentParamKey][searchParamKey] : e2[parentParamKey],
+    const a = searchParamKey
+        ? e1[parentParamKey][searchParamKey]
+        : e1[parentParamKey],
+      b = searchParamKey
+        ? e2[parentParamKey][searchParamKey]
+        : e2[parentParamKey],
       sortOrder = direction === "asc" ? 1 : -1;
     return a < b ? -sortOrder : a > b ? sortOrder : 0;
   };
 
-const startSort = (initialArray, parentParamKey, searchParamKey, direction, descTxt, sortParamTxt, measure) => {
+const startSort = (
+  initialArray,
+  parentParamKey,
+  searchParamKey,
+  direction = "asc",
+  measure = ""
+) => {
   resultList.textContent = "";
   const resultHeader = document.createElement("h6");
   resultList.append(resultHeader);
-  resultHeader.textContent = descTxt;
-  const result = []
+  let sortDirection = "";
+  if (direction === "asc") {
+    sortDirection = "low to high";
+  } else sortDirection = "high to low";
+  resultHeader.textContent = `Sorted ${sortDirection} by ${searchParamKey}`;
+
+  const result = [];
   let sortingArray = [...initialArray];
   sortingArray.sort(nestedSort(parentParamKey, searchParamKey, direction));
   for (let i = 0; i < sortingArray.length; i++) {
-    if (sortingArray[i].params[searchParamKey] === sortingArray[0].params[searchParamKey]) {
+    if (
+      sortingArray[i].params[searchParamKey] ===
+      sortingArray[0].params[searchParamKey]
+    ) {
       result.push(sortingArray[i]);
       const newResultP = document.createElement("p");
-      newResultP.textContent = `${sortingArray[i].name}. ${sortParamTxt} ${sortingArray[i].params[searchParamKey]} ${measure}`;
+      newResultP.textContent = `${sortingArray[i].name}. ${sortingArray[i].params[searchParamKey]} ${measure}`;
       resultList.append(newResultP);
-    } 
+    }
   }
 };
 
-btnShowSmall.onclick = () => {
-  startSort(state, "params", "height", "asc", "Самый мелкий:", "Рост:", "см");
+searchBtn.onclick = () => {
+  const arrayName = document.getElementById("array-name").value;
+  const objectName = document.getElementById("object-name").value;
+  const searchParam = document.getElementById("search-param").value;
+  const searchDirection = document.getElementById("search-direction").value;
+  const measure = document.getElementById("measure").value;
+
+  startSort(eval(arrayName), objectName, searchParam, searchDirection, measure);
 };
-
-btnShowTall.onclick = () => {
-  startSort(state, "params", "height", "desc", "Самый высокий:", "Рост:", "см");
-};
-
-btnShowThin.onclick = () => {
-  startSort(state, "params", "weight", "asc", "Самый тощий:", "Вес:", "кг");
-};
-
-btnShowFat.onclick = () => {
-  startSort(state, "params", "weight", "desc", "Самый толстый:", "Вес:", "кг");
-};
-
-
-
